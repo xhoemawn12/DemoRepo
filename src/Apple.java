755c960f99b0fcda0b4a9a373aa498fe6f4972ac
RiRi <RiRi>
2016-05-21 15:47:16 +0545

    Added methods

--- src/Apple.java
+++ src/Apple.java
@@ -1,5 +1,9 @@
 public class Apple{
   public static void main(String[] args){
     System.out.println("Just a Demo");
+    Apple a = new Apple().myMethod();
+  }
+  public void myMethod(){
+    System.exit(0);
   }
 }
